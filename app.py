8ac935cb3a066d65822e9d33aef374745de26ec2
Dawit Shibabaw <61491978+dawitemu1@users.noreply.github.com>
2024-01-02 15:18:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ if st.button("Predict type of Iris"):
     result = predict(
         np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
    if (result[0]==0):
-        species = 'Iris-setosa'
+         species = 'Iris-setosa'
     else if (result[0] == 1):
-   species = 'Iris-versicolor'
+          species = 'Iris-versicolor'
         else:
           species = 'Iris-virginica'
         
